55be927e6784cb504906fa4f136e9e7094f2ff1a
Enrique <moralesagurto.0803@gmail.com>
2023-09-04 21:31:54 -0500

    footer-version-final

--- js/main.js
+++ js/main.js
@@ -48,6 +48,17 @@ juegos.forEach(juego => {
     juegosLista.appendChild(listItem);
 });
   
-  
-  
+const contactoForm = document.getElementById('contacto-form');
+
+contactoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const email = document.getElementById("email").value;
+  const mensaje = document.getElementById("mensaje").value;
+
+  alert('Gracias por contactarnos - Nombre: ' + nombre + '- Email: ' + email + '- Mensaje: ' + mensaje);
+
+  contactoForm.reset();
+});
   
